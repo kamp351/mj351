351cab842171aa50ef849e0e2ce19d27254f28d8
kamp351 <57995064+kamp351@users.noreply.github.com>
2019-11-20 19:28:58 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,33 @@
+# for i in range (1,5):
+#     for j in range (1,5):
+#         print(j,end=" ")
+#     print()
+
+# for i in range (1,5):
+#     for j in range (1,5):
+#         print(i, end= " ")
+#     print()
+
+# for i in range (1,5):
+#     for j in range (1, (i+1)):
+#         print("*",end="")
+#     print()
+q = 0
+# for i in range (5,1,-1):
+#      # print(5)
+#      q+=1
+#      for j in range (5,q,-1):
+#
+#          print(j,end=" ")
+#      print()
+
+# for i in range (1,5):
+#
+#     for j in range (1,(i+1)):
+#         print(i,end=" ")
+#
+#     print()
+# for i in range (1,5):
+#     for j in range (1,(i+1)):
+#         print(j,end=' ')
+#     print()
